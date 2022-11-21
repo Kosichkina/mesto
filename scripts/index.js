507b58f01889e6346ad6a8ed60376ae6163e5961
Natalya <natal1.andreevna@yandex.ru>
2022-11-21 14:41:20 +0300

    ПР4

--- scripts/index.js
+++ scripts/index.js
@@ -3,31 +3,25 @@
 /*выбор DOM -элементов*/
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-
-
-
-/*Регистрация обработчика событий по клику */
-
-
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupInputName = popupElement.querySelector('.popup__input_type_name');
 const popupInputDescription = popupElement.querySelector('.popup__input_type_description');
 const profileElement = document.querySelector('.profile');
 const profileTitleElement = profileElement.querySelector('.profile__title');
-const profileSubtitleElement = profileElement.querySelector('.profile__subtitle');
+const profileSubtitleElement = profileElement.querySelector('.profile__info');
 const formElement = document.querySelector('.popup__form');
 
 
 const openPopup = function () {
-    popupElement.classList.add('popup_is-opened');
+    popupElement.classList.add('popup_active');
     popupInputName.value = profileTitleElement.textContent;
     popupInputDescription.value = profileSubtitleElement.textContent;
 };
 
 const closePopup = function () {
-    popupElement.classList.remove('popup_is-opened');
+    popupElement.classList.remove('popup_active');
 };
-
+/*Регистрация обработчика событий по клику */
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
