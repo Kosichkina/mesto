72e6bd7ebcfae101f1636716a8044972d8a46dbb
Natalya <natal1.andreevna@yandex.ru>
2023-01-11 15:18:11 +0300

    ПР-6-ревью-3

--- scripts/index.js
+++ scripts/index.js
@@ -6,16 +6,16 @@ const popupCloseButtonProfileElement = popupEditProfile.querySelector('.popup__c
 const popupOpenButtonProfileElement = document.querySelector('.profile__edit-button');
 const popupInputName = popupEditProfile.querySelector('.popup__input_type_name');
 const popupInputDescription = popupEditProfile.querySelector('.popup__input_type_description');
-const profileElement = document.querySelector('.profile'); 
+const profileElement = document.querySelector('.profile');
 const profileTitleElement = profileElement.querySelector('.profile__title');
 const profileSubtitleElement = profileElement.querySelector('.profile__info');
 const popupformEditProfile = popupEditProfile.querySelector('.popup__form_edit-profile');
 const popupForm = document.querySelector('.popup__form');
-  
+
 
 //открытие попапа добавления картинки - выбор переменных
 const popupNewPlaceElement = document.querySelector('.popup_type_new-place');
-const buttonOpenPopupAdd= document.querySelector('.profile__add-button');
+const buttonOpenPopupAdd = document.querySelector('.profile__add-button');
 const popupInputNewPlace = popupNewPlaceElement.querySelector('.popup__input_type_place');
 const popupInputLinkPlace = popupNewPlaceElement.querySelector('.popup__input_type_link');
 const buttonClosePopupAdd = popupNewPlaceElement.querySelector('.popup__close');
@@ -27,6 +27,9 @@ const popupFormAdd = popupNewPlaceElement.querySelector('.popup__form-new-place'
 /*
 const openPopup = function (popup) { 
     popup.classList.add('popup_active');
+
+    в данном случае функция закрытия попапа принимает в качестве параметра элемент попапа.
+     Вот мы его в начале находим, а затем передаём в функцию closePopup
 }
 const closePopup = function(popup) {
     popup.classList.remove('popup_active')
@@ -44,73 +47,84 @@ const closePopupEsc = (event) => {
 
 //закрытие попапа OVERLAY
 
-function closePopupOverlay(event){
-  if(event.target === event.currentTarget) {
+function closePopupOverlay(event) {
+  if (event.target === event.currentTarget) {
     closePopup(event.target);
   };
-  
+
 }
 
 //открытие попапа
-const openPopup = function(popup) {
- popup.classList.add('popup_active');
+const openPopup = function (popup) {
+  popup.classList.add('popup_active');
   document.addEventListener('keydown', closePopupEsc);
   popup.addEventListener("click", closePopupOverlay)
 };
 
 //закрытие попапа
+//мы объявляем функцию и сохраняем её в переменную. 
+//popup - это параметр функции, который передаётся в момент её вызова
+
 const closePopup = function (popup) {
   popup.classList.remove('popup_active');
-  document.removeEventListener('keydown', closePopupEsc);  
+  document.removeEventListener('keydown', closePopupEsc);
   popup.removeEventListener("click", closePopupOverlay)
 };
 
 
 //Открытие и закрытие редактирования профиля
-popupOpenButtonProfileElement.addEventListener('click', function(){
-    openPopup(popupEditProfile);
-    popupInputName.value = profileTitleElement.textContent;
-    popupInputDescription.value = profileSubtitleElement.textContent;
-  });
-  
-  popupCloseButtonProfileElement.addEventListener('click', function() {
-    closePopup(popupEditProfile);
+//программирование кнопки
+popupOpenButtonProfileElement.addEventListener('click', function () {
+  openPopup(popupEditProfile);
+  popupInputName.value = profileTitleElement.textContent;
+  popupInputDescription.value = profileSubtitleElement.textContent;
+});
+
+popupCloseButtonProfileElement.addEventListener('click', function () {
+  closePopup(popupEditProfile);
 
 
-  });
-  
-  const handleSubmitProfile = (event) => {
-    event.preventDefault();
-    profileTitleElement.textContent = popupInputName.value;
-    profileSubtitleElement.textContent = popupInputDescription.value;
-    closePopup(popupEditProfile);
-  };
+});
+//Заполнение полей формы и программирование кноки Submit/сохранить/ добавить. Submit -это встроенное событие
+const handleSubmitProfile = (event) => {
+  event.preventDefault(); // чтобы не перегружалась страница 
+  profileTitleElement.textContent = popupInputName.value;
+  profileSubtitleElement.textContent = popupInputDescription.value;
+  closePopup(popupEditProfile);
+};
+// добавляем слушатель на нажатие кнопки "сохранить"+ заполнение полей
+popupformEditProfile.addEventListener('submit', handleSubmitProfile);
 
-  popupformEditProfile.addEventListener('submit', handleSubmitProfile);
-  
-  
-  //Открытие и закрытие добавления карточки
-  buttonOpenPopupAdd.addEventListener('click', function() {
-    openPopup(popupNewPlaceElement);
-    popupFormAdd.reset();
+
+//Открытие и закрытие добавления карточки
+buttonOpenPopupAdd.addEventListener('click', function () {
+  openPopup(popupNewPlaceElement);
+  popupFormAdd.reset();
+});
+
+buttonClosePopupAdd.addEventListener('click', function () {
+  closePopup(popupNewPlaceElement);
+});
+
+
+const handleSubmitAddCard = (event) => {
+  event.preventDefault();
+  event.submitter.disabled = true;
+  event.submitter.classList.add('popup__button_invalid');
+  renderCard({
+    name: popupInputNewPlace.value,
+    link: popupInputLinkPlace.value,
   });
-  
-  buttonClosePopupAdd.addEventListener('click', function() {
-    closePopup(popupNewPlaceElement);
-  });
-  
-  const handleSubmitAddCard = (event) => {
-      event.preventDefault();
-      event.submitter.disabled = true;
-      event.submitter.classList.add('popup__button_invalid');
-      renderCard({
-          name: popupInputNewPlace.value,
-          link: popupInputLinkPlace.value,
-      });
-      closePopup(popupNewPlaceElement);
-  };
-  
-  popupFormAdd.addEventListener('submit', handleSubmitAddCard);
+  closePopup(popupNewPlaceElement);
+};
+
+popupFormAdd.addEventListener('submit', handleSubmitAddCard);
+/*У каждого элемента есть метод addEventListener, 
+благодаря которому мы для события (первый параметр) назначаем функцию (2ой параметр)
+когда произойдёт клик по кнопке у которой type="submit" то произойдёт событие с "sibmit" 
+и выполнятся все функции-обработчики, 
+которые были подписаны на это событие через addEventListener
+*/
 
 //Создание карточки
 
@@ -122,7 +136,7 @@ const сardTemplate = document.querySelector('#places-template').content.querySe
 const popupZoom = document.querySelector('.popup_type_zoom');
 const popupImageContainer = document.querySelector('.popup__image-container');
 const popupOpenBigImage = popupImageContainer.querySelector('.popup-zoomm');
-const popupCloseZoom = popupImageContainer.querySelector('.popup__close'); 
+const popupCloseZoom = popupImageContainer.querySelector('.popup__close');
 const popupImage = document.querySelector('.places__img');
 const popupOpenZoom = popupImageContainer.querySelector('.popup__img-zoom');
 const popupImageCapture = popupImageContainer.querySelector('.popup__capture-zoom');
@@ -130,55 +144,55 @@ const popupImageCapture = popupImageContainer.querySelector('.popup__capture-zoo
 // закончились переменные с большой картинкой
 
 const createCard = (card) => {
-    const сardNew = сardTemplate.cloneNode(true);
-    const сardTitle = сardNew.querySelector('.places__capture');
-    const cardImage = сardNew.querySelector('.places__img');
-    const cardDeleteButton = сardNew.querySelector('.places__delete-button');
-    const cardLikeButton = сardNew.querySelector('.places__like');
+  const сardNew = сardTemplate.cloneNode(true);
+  const сardTitle = сardNew.querySelector('.places__capture');
+  const cardImage = сardNew.querySelector('.places__img');
+  const cardDeleteButton = сardNew.querySelector('.places__delete-button');
+  const cardLikeButton = сardNew.querySelector('.places__like');
 
-    сardTitle.textContent = card.name;
-    cardImage.src = card.link;
-    cardImage.alt = card.name;
+  сardTitle.textContent = card.name;
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
 
-    cardDeleteButton.addEventListener('click', handleDeleteButtonClick);
-    cardLikeButton.addEventListener('click', handleLikeButtonClick);
-    cardImage.addEventListener('click', handlepopupOpenBigImageClick);
+  cardDeleteButton.addEventListener('click', handleDeleteButtonClick);
+  cardLikeButton.addEventListener('click', handleLikeButtonClick);
+  cardImage.addEventListener('click', handlepopupOpenBigImageClick);
 
-    return сardNew;
+  return сardNew;
 
 };
 const handleDeleteButtonClick = (e) => {
-    e.target.closest('.places__item').remove()
+  e.target.closest('.places__item').remove()
 }
 /*стрелочная функция по мотивам вебинара Максима Чечелева */
 
 const handleLikeButtonClick = function (evt) {
-    evt.target.classList.toggle('places__like_active')
+  evt.target.classList.toggle('places__like_active')
 };
 /*в toggle (переключатель) перередается только селектор, не класс, поэтому пишется без точки*/
 
 const handlepopupOpenBigImageClick = (e) => {
-    openPopup(popupZoom);
+  openPopup(popupZoom);
 
-    popupOpenZoom.src = e.target.src;
-    popupOpenZoom.alt = e.target.alt;
-    popupImageCapture.textContent = e.target.alt;
-    /*
-    popupImageCapture.textContent = e.target.closest('.places__item').querySelector('.places__capture').textContent;
+  popupOpenZoom.src = e.target.src;
+  popupOpenZoom.alt = e.target.alt;
+  popupImageCapture.textContent = e.target.alt;
+  /*
+  popupImageCapture.textContent = e.target.closest('.places__item').querySelector('.places__capture').textContent;
 */
-  };
-  
-  
-  popupCloseZoom.addEventListener('click', function() {
-    closePopup(popupZoom);
+};
+
+
+popupCloseZoom.addEventListener('click', function () {
+  closePopup(popupZoom);
 });
 
 
 const renderCard = (card) => {
-    cardsContainer.prepend(createCard(card));
+  cardsContainer.prepend(createCard(card));
 };
 
 initialCards.forEach((card) => {
-    renderCard(card);
+  renderCard(card);
 });
 
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,96 +1,86 @@
 
 // Добавление валидации
-  
-const  validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_invalid',
-    errorClass: 'popup__input-error_visible', //нет класса в htm, есть в css
-  };
 
-  enableValidation(validationConfig)
-  
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_invalid',
+  errorClass: 'popup__input-error_visible', //нет класса в htm, есть в css
+};
 
-  /*const enableValidation = (config) => {
-    const forms =[...document.querySelectorAll(config.formselector)]
-  }
-  */
+enableValidation(validationConfig)
+
+
+/*const enableValidation = (config) => {
+  const forms =[...document.querySelectorAll(config.formselector)]
+}
+*/
 function showInputError(formElement, inputElement, config) {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    errorElement.classList.add(config.errorClass);
-    errorElement.textContent = inputElement.validationMessage;
-    inputElement.classList.add(config.inputErrorClass);
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.add(config.errorClass);
+  errorElement.textContent = inputElement.validationMessage;
+  inputElement.classList.add(config.inputErrorClass);
 }
 
 function hideInputError(formElement, inputElement, config) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    errorElement.classList.remove(config.errorClass);
-    errorElement.textContent = '';
-    inputElement.classList.remove(config.inputErrorClass);  
+  errorElement.classList.remove(config.errorClass);
+  errorElement.textContent = '';
+  inputElement.classList.remove(config.inputErrorClass);
 }
 
-  function checkInputValidity(formElement, inputElement, config) {
-   if (inputElement.validity.valid) {
+function checkInputValidity(formElement, inputElement, config) {
+  if (inputElement.validity.valid) {
     hideInputError(formElement, inputElement, config);
-} 
+  }
   else {
-   showInputError(formElement, inputElement, config);
+    showInputError(formElement, inputElement, config);
+  }
 }
-   }
 
-   function hasInvalidInput(inputList) {
-    return inputList.some((inputElement) => !inputElement.validity.valid);
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => !inputElement.validity.valid);
+
+}
+
+function toggleButtonState(inputList, buttonElement, config) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(config.inactiveButtonClass);
+    buttonElement.disabled = true;
+
+
+  } else {
+    buttonElement.classList.remove(config.inactiveButtonClass);
+    buttonElement.disabled = false;
 
   }
-  //функции для скрытия кнопки  
-  function disableSubmithButton(buttonElement, config) {
-    buttonElement.classList.add('inactiveButtonClass');
-    buttonElement.disabled = 'disabled';
-  }
+}
 
-  function enableSubmithButton(buttonElement, config) {
-    buttonElement.classList.remove('inactiveButtonClass');
-    buttonElement.disabled = '';
-  }
-
-  function toggleButtonState(inputList, buttonElement, config) {
-    if (hasInvalidInput(inputList)) {
-      buttonElement.classList.add(config.inactiveButtonClass);
-      buttonElement.disabled = true;
-     
-      
-    } else {
-      buttonElement.classList.remove(config.inactiveButtonClass);
-      buttonElement.disabled = false;
-
-    }
-  }
-  
 
 function setEventListeners(formElement, config) {
-const inputList = Array.from(formElement.querySelectorAll(config.inputSelector)); 
-const buttonElement = formElement.querySelector(config.submitButtonSelector)
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector)
 
-toggleButtonState(inputList, buttonElement, config)
+  toggleButtonState(inputList, buttonElement, config)
 
-inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input',() => {
-        checkInputValidity(formElement, inputElement, config);
-        toggleButtonState(inputList, buttonElement, config);
-      })
-
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
     })
-    
+
+  })
+
 }
 
-function enableValidation({ formSelector, ...restConfig }){
-const formList = Array.from(document.querySelectorAll(formSelector));
+function enableValidation({ formSelector, ...restConfig }) {
+  const formList = Array.from(document.querySelectorAll(formSelector));
 
-formList.forEach((formElement) => {
+  formList.forEach((formElement) => {
     setEventListeners(formElement, restConfig)
-})
- }
+  })
+}
 
 /*
  не вызывать toggleButtonState вне обработчика события input, а завести в validate.js
